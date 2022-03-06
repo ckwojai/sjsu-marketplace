d1b0998aa7ed908dd1ec3fb44fdeb973c739544c
Kin Chang <kinchang@kins-mbp.lan>
2022-03-06 09:37:31 -0800

    typo in prereq script

--- install_prereq_linux.sh
+++ install_prereq_linux.sh
@@ -2,7 +2,7 @@
 install_docker() {
     sudo apt-get remove docker docker-engine docker.io containerd runc
     sudo apt-get update
-    sudo apt-get install ca-certificates curl gnupg lsb-releasesudo --yes
+    sudo apt-get install ca-certificates curl gnupg lsb-release --yes
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
