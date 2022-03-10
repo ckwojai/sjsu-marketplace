a0693a225c285c93fc677805c18ef3f4cc5e1b69
Kin Chang <kinchang@kins-mbp.lan>
2022-03-10 01:25:30 -0800

    get rid of fancy stuff

--- start_dev.sh
+++ start_dev.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-OS="$(uname -a)"
-if echo ${OS} | grep Ubuntu > /dev/null 2>&1; then
-    OS=Linux
-elif echo ${OS} | grep Darwin > /dev/null 2>&1; then
-    OS=MacOS
-else
-    echo "Unsupported OS: ${OS}. Exiting..."
-    exit 2
-fi
+# OS="$(uname -a)"
+# if echo ${OS} | grep Ubuntu > /dev/null 2>&1; then
+#     OS=Linux
+# elif echo ${OS} | grep Darwin > /dev/null 2>&1; then
+#     OS=MacOS
+# else
+#     echo "Unsupported OS: ${OS}. Exiting..."
+#     exit 2
+# fi
 
 minikube dashboard &
-. config_hosts.sh
+# . config_hosts.sh
 minikube tunnel &
 
-sleep 3
-if [ ${OS} == "MacOS" ]; then
-    open http://sjsump.ai
-elif [ ${OS} == "Linux" ]; then
-    xdg-open http://sjsump.ai
-else
-    echo "Unsupported OS: ${OS}. Exiting..."
-    exit 2
-fi
+# sleep 3
+# if [ ${OS} == "MacOS" ]; then
+#     open http://sjsump.ai
+# elif [ ${OS} == "Linux" ]; then
+#     xdg-open http://sjsump.ai
+# else
+#     echo "Unsupported OS: ${OS}. Exiting..."
+#     exit 2
+# fi
